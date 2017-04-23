f020f49d8553e7b7a19a3216e249cde84abf987c
Daniel Sousa <sousa.dfs@gmail.com>
2017-04-23 00:41:44 -0300

    Add cron job script to update the magnet links

--- src/services/cron.js
+++ src/services/cron.js
@@ -1,27 +1,68 @@
-import { spawn, exec } from 'child_process';
+#!/usr/bin/env node
+
 import rarbg from 'rarbg';
 import TvShow from '../api/show/model';
+import mongoose from './mongoose';
+import config from '../config';
+import filter from '../util/download';
 
-function updateTvShow(data) {
-    // TODO
+mongoose.connect(config.mongo.uri);
+
+// List of updated Tv Shows
+const updated = [];
+
+function updateTvShow(show) {
+  return function setData(data) {
+    /* eslint-disable */
+    show.download = [];
+    /* eslint-enable */
+    data.forEach(filteredObj => show.download.push(filteredObj));
+    // Return save mongoose model promise
+    return show.save();
+  };
+}
+
+function filterData(data) {
+  return Promise.resolve(filter(data));
 }
 
 function searchNewLinks(show) {
-  rarbg.search({
+  return rarbg.search({
     search_string: `${show.current_season} 1080p`,
-    search_imdb: show.imdbid,
+    search_imdb: show.imdbID,
     category: rarbg.categories.TV_HD_EPISODES,
     sort: 'seeders',
-  })
-    .then(updateTvShow);
-}
-
-function loopShows(shows) {
-  shows.forEach((show) => {
-    searchNewLinks(show);
   });
 }
 
+function run(shows) {
+  // List of Promises
+  const promises = [];
+
+  shows.forEach((show) => {
+    // Creates a Promise for each Show
+    const p = new Promise((resolve, reject) => {
+      searchNewLinks(show)
+        .then(filterData)
+        .then(updateTvShow(show))
+        .then(() => updated.push(show.name))
+        .then(() => resolve())
+        .catch((err) => {
+          console.log(err);
+          reject();
+        });
+    });
+    // Add current promise to the List
+    promises.push(p);
+  });
+
+  // Return all Promises
+  return Promise.all(promises);
+}
+
 TvShow.find({})
-    .then(loopShows)
-    .catch(err => console.log(err));
+  .then(run)
+  .then(() => updated.map(name => console.log(`\n${name} updated.`)))
+  .then(process.exit)
+  .catch(err => console.log(err));
+

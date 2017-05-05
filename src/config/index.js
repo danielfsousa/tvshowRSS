eebbdd79e9220005a1eae36ecfb849d331a96e3b
Daniel Sousa <sousa.dfs@gmail.com>
2017-05-05 13:53:31 -0300

    Fix: load .env variables before config

--- src/config/index.js
+++ src/config/index.js
@@ -5,11 +5,11 @@
 
 import { merge } from 'lodash';
 import path from 'path';
+import nodeEnv from '../util/env';
 import app from './app';
 import logs from './logs';
 import rss from './rss';
 import environment from './environment';
-import nodeEnv from '../util/env';
 
 // exports merged configuration object
 module.exports = merge(app, rss, logs, environment);
